0647e0e70e872f5857db617083db4f974f84ebf2
Ramkumar Pillai <psramkumar@gmail.com>
2012-12-24 00:40:26 -0600

    Themes and Templates

--- src/main/java/com/ttech/advn/prj/mbean/UserManagedBean.java
+++ src/main/java/com/ttech/advn/prj/mbean/UserManagedBean.java
@@ -4,7 +4,10 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
 import javax.faces.model.SelectItem;
 
 import lombok.Data;
@@ -64,73 +67,91 @@ public class UserManagedBean implements Serializable {
 		userList.addAll(userService.getUsers());
 		return userList;
 	}
-	
-	 private DynaFormModel model;
 
-	 private static List<SelectItem> LANGUAGES = new ArrayList<SelectItem>();
-	 
-	 public UserManagedBean(){
-		 model = new DynaFormModel();
+	private DynaFormModel model;
 
-	      // add rows, labels and editable controls
-	      // set relationship between label and editable controls to support outputLabel with "for" attribute
+	private static List<SelectItem> LANGUAGES = new ArrayList<SelectItem>();
 
-	      // 1. row
-	      DynaFormRow row = model.createRegularRow();
 
-	      DynaFormLabel label11 = row.addLabel("Author", 1, 1);
-	      DynaFormControl control12 = row.addControl(new BookProperty("Author", true), "input", 1, 1);
-	      label11.setForControl(control12);
+	@PostConstruct
+	public void init(){
+		model = new DynaFormModel();
 
-	      DynaFormLabel label13 = row.addLabel("ISBN", 1, 1);
-	      DynaFormControl control14 = row.addControl(new BookProperty("ISBN", true), "input", 1, 1);
-	      label13.setForControl(control14);
+		// add rows, labels and editable controls
+		// set relationship between label and editable controls to support outputLabel with "for" attribute
 
-	      // 2. row
-	      row = model.createRegularRow();
+		// 1. row
+		DynaFormRow row = model.createRegularRow();
 
-	      DynaFormLabel label21 = row.addLabel("Title", 1, 1);
-	      DynaFormControl control22 = row.addControl(new BookProperty("Title", false), "input", 3, 1);
-	      label21.setForControl(control22);
+		DynaFormLabel label11 = row.addLabel("Author", 1, 1);
+		DynaFormControl control12 = row.addControl(new BookProperty("Author", true), "input", 1, 1);
+		label11.setForControl(control12);
 
-	      // 3. row
-	      row = model.createRegularRow();
+		DynaFormLabel label13 = row.addLabel("ISBN", 1, 1);
+		DynaFormControl control14 = row.addControl(new BookProperty("ISBN", true), "input", 1, 1);
+		label13.setForControl(control14);
 
-	      DynaFormLabel label31 = row.addLabel("Publisher", 1, 1);
-	      DynaFormControl control32 = row.addControl(new BookProperty("Publisher", false), "input", 1, 1);
-	      label31.setForControl(control32);
+		// 2. row
+		row = model.createRegularRow();
 
-	      DynaFormLabel label33 = row.addLabel("Published on", 1, 1);
-	      DynaFormControl control34 = row.addControl(new BookProperty("Published on", false), "calendar", 1, 1);
-	      label33.setForControl(control34);
+		DynaFormLabel label21 = row.addLabel("Title", 1, 1);
+		DynaFormControl control22 = row.addControl(new BookProperty("Title", false), "input", 3, 1);
+		label21.setForControl(control22);
 
-	      // 4. row
-	      row = model.createRegularRow();
+		// 3. row
+		row = model.createRegularRow();
 
-	      DynaFormLabel label41 = row.addLabel("Language", 1, 1);
-	      DynaFormControl control42 = row.addControl(new BookProperty("Language", false), "select", 1, 1);
-	      label41.setForControl(control42);
+		DynaFormLabel label31 = row.addLabel("Publisher", 1, 1);
+		DynaFormControl control32 = row.addControl(new BookProperty("Publisher", false), "input", 1, 1);
+		label31.setForControl(control32);
 
-	      DynaFormLabel label43 = row.addLabel("Description", 1, 2);
-	      DynaFormControl control44 = row.addControl(new BookProperty("Description", false), "textarea", 1, 2);
-	      label43.setForControl(control44);
+		DynaFormLabel label33 = row.addLabel("Published on", 1, 1);
+		DynaFormControl control34 = row.addControl(new BookProperty("Published on", false), "calendar", 1, 1);
+		label33.setForControl(control34);
 
-	      // 5. row
-	      row = model.createRegularRow();
+		// 4. row
+		row = model.createRegularRow();
 
-	      DynaFormLabel label51 = row.addLabel("Rating", 1, 1);
-	      DynaFormControl control52 = row.addControl(new BookProperty("Rating", 3, true), "rating", 1, 1);
-	      label51.setForControl(control52);
+		DynaFormLabel label41 = row.addLabel("Language", 1, 1);
+		DynaFormControl control42 = row.addControl(new BookProperty("Language", false), "select", 1, 1);
+		label41.setForControl(control42);
+
+		DynaFormLabel label43 = row.addLabel("Description", 1, 2);
+		DynaFormControl control44 = row.addControl(new BookProperty("Description", false), "textarea", 1, 2);
+		label43.setForControl(control44);
+
+		// 5. row
+		row = model.createRegularRow();
+
+		DynaFormLabel label51 = row.addLabel("Rating", 1, 1);
+		DynaFormControl control52 = row.addControl(new BookProperty("Rating", 3, true), "rating", 1, 1);
+		label51.setForControl(control52);
 	}
-	
-	 public List<SelectItem> getLanguages() {
-	        if (LANGUAGES.isEmpty()) {
-	            LANGUAGES.add(new SelectItem("en", "English"));
-	            LANGUAGES.add(new SelectItem("de", "German"));
-	            LANGUAGES.add(new SelectItem("ru", "Russian"));
-	            LANGUAGES.add(new SelectItem("tr", "Turkish"));
-	        }
 
-	        return LANGUAGES;
-	    }
+	public List<SelectItem> getLanguages() {
+		if (LANGUAGES.isEmpty()) {
+			LANGUAGES.add(new SelectItem("en", "English"));
+			LANGUAGES.add(new SelectItem("de", "German"));
+			LANGUAGES.add(new SelectItem("ru", "Russian"));
+			LANGUAGES.add(new SelectItem("tr", "Turkish"));
+		}
+
+		return LANGUAGES;
+	}
+	public void save() {
+		addMessage("Data saved");
+	}
+
+	public void update() {
+		addMessage("Data updated");
+	}
+
+	public void delete() {
+		addMessage("Data deleted");
+	}
+
+	public void addMessage(String summary) {
+		FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, summary,  null);
+		FacesContext.getCurrentInstance().addMessage(null, message);
+	}
 }
